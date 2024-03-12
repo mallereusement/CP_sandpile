c13598dd898f30521b01c25ffcb71d617ff739e2
Sam <mr.explorer61@gmail.com>
2024-03-12 10:35:22 +0100

    comments

--- calc_exponents.py
+++ calc_exponents.py
@@ -407,7 +407,7 @@ if __name__ == '__main__':
 
         fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [3, 1]}, figsize=(8, 6), sharex=True)
 
-        # Plot main data and model
+     
         ax1.errorbar(result["x"], result["data"], yerr=result["errors"], fmt='o', color='blue', capsize=3, markersize=4, label='Simulation Data', zorder=1)
         ax1.plot(result["x"], result["model"], color="orange", linewidth=2, label="Model", zorder=2)
         ax1.set_yscale('log')
@@ -415,12 +415,12 @@ if __name__ == '__main__':
         ax1.legend()
         ax1.set_ylabel(analysis_parameter[ana_para]['ylabel'])
 
-        # Calculate errors divided by difference between data and model
+        
         error_ratio = (result["data"] - result["model"]) / result["errors"]
 
-        # Plot error ratio
+    
         ax2.scatter(result["x"], error_ratio, color='red')
-        ax2.set_xscale('log')  # Set x-axis scale to logarithmic
+        ax2.set_xscale('log')
         ax2.set_xlabel(analysis_parameter[ana_para]['xlabel'])
         ax2.set_ylabel("(Data - Model) / Errors")
         ax2.set_ylim(-5, 5)
